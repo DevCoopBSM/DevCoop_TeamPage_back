26816c5cbe6f5dc65917917be390411513358ea0
myowww <eahenlee@gmail.com>
2023-09-21 19:57:54 +0900

    ERROR :: error

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const db = require("./models");
-const port = 2000;
-const boardRouter = require("./routes/boardRouter.js");
-const join = require("./routes/join.js");
-const login = require("./routes/login.js");
+const db = require('./models');
+const port = 1000;
+const boardRouter = require('./routes/boardRouter.js');
+const join = require('./routes/join.js');
+const login = require('./routes/login.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", boardRouter);
-app.use("/api", join);
-app.use("/api", login);
+app.use('/api', boardRouter);
+app.use('/api', join);
+app.use('/api', login);
 
 db.sequelize
   .sync()
   .then(() => {
-    console.log("db connect");
+    console.log('db connect');
   })
   .catch(console.error);
 
--- src/routes/boardRouter.js
+++ src/routes/boardRouter.js
@@ -1,8 +1,33 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { Sequelize, sequelize, DataTypes } = require("sequelize");
-const models = require("../models");
+const { Sequelize, sequelize, DataTypes } = require('sequelize');
+const models = require('../models');
+const boardList = models.board;
 
 router.use(express.json());
+
+router.put('/create', async (req, res) => {
+  const id = req.params.id;
+  const condition = id ? { where: { id: id } } : null;
+
+  boardList
+    .update(req.body, condition)
+    .then((resultCount) => {
+      if (resultCount == 1) {
+        res.send({
+          message: `정상적으로 게시물이 업데이트 되었습니다. (id: ${id} )`,
+        });
+      } else {
+        res.send({
+          message: `Update Error (id: ${id} )`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || `Update Error (id: ${id} )`,
+      });
+    });
+});
 
 module.exports = router;
