ebd594f6fc57eb41a3e09d30e8a92a45171b7ce3
myowww <eahenlee@gmail.com>
2023-09-21 22:39:49 +0900

    ERROR :: error

--- src/routes/boardRouter.js
+++ src/routes/boardRouter.js
@@ -6,28 +6,26 @@ const boardList = models.board;
 
 router.use(express.json());
 
-router.put('/create', async (req, res) => {
+router.put('/update/:id', async (req, res) => {
   const id = req.params.id;
-  const condition = id ? { where: { id: id } } : null;
+  const condition = { where: { board_id: id } };
 
-  boardList
-    .update(req.body, condition)
-    .then((resultCount) => {
-      if (resultCount == 1) {
-        res.send({
-          message: `정상적으로 게시물이 업데이트 되었습니다. (id: ${id} )`,
-        });
-      } else {
-        res.send({
-          message: `Update Error (id: ${id} )`,
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || `Update Error (id: ${id} )`,
+  try {
+    const [resultCount] = await boardList.update(req.body, condition);
+    if (resultCount > 0) {
+      res.send({
+        message: `정상적으로 게시물이 업데이트 되었습니다. (id: ${id} )`,
       });
+    } else {
+      res.send({
+        message: `Update Error (id: ${id} )`,
+      });
+    }
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || `Update Error (id: ${id} )`,
     });
+  }
 });
 
 module.exports = router;
