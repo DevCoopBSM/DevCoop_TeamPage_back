9e3c7f4b76651849dd66f6eea3a250293ad329c1
hongseop <a01082372487@gmail.com>
2023-09-21 17:31:57 +0900

    ADD :: login.js랑 연결

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ const app = express();
 const db = require("./models");
 const port = 8000;
 const boardRouter = require("./routes/boardRouter.js");
-const join = require("./routes/join,js");
+const join = require("./routes/join.js");
+const login = require("./routes/login.js");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", boardRouter, join);
+app.use("/api", boardRouter, join, login);
 
 db.sequelize
   .sync()
