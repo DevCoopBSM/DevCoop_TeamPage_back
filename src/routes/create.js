f56a1b8fa76f7ea0a40e35d460227244dca8c986
KMS <13hj45@gmail.com>
2023-10-09 02:00:05 +0900

    create 기능

--- src/routes/create.js
+++ src/routes/create.js
@@ -6,22 +6,29 @@ const boardList = models.board;
 
 router.use(express.json());
 
-router.post('/create/:id', async(req, res) => {
-  const id = req.params.id;
-  try{
-    if(!id){
-        res.send({
-            message: `ID가 일치하지 않습니다. (id: ${id} )`,
-          });
-    }
-    else{
-        res.send({
-            message: `성공 (id: ${id} )`,
-          });
-    }
-  } catch(err){
-    res.status(500).send({
-        message: err.message || `Create Error (id: ${id} )`,
+router.post('/api/create', async (req, res) => {
+  const { id } = req.body;
+  try {
+    if (!id) {
+      res.send({
+        message: '관리자 권한을 확인해 주세요.',
       });
+    } else {
+        const { title, content } = req.body;
+
+        const newBoard = await boardList.create({
+          title,
+          content,
+        });  
+        res.send({
+          message: '공지글로 등록 하시겠어요?',
+        });
+    }
+  } catch (error) {
+    res.status(500).send({
+      message: error.message || `Create Error (id: ${id} )`,
+    });
   }
 });
+
+module.exports = router;
